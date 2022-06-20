12116d56642caec41863504afbb4009fc5d84cde
Adam Załęski <adamzaleski001@wp.pl>
2022-06-20 13:44:04 +0200

    Fixing adding books to library, getting data from the form  and properly displaying books in a table

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let myLibrary = [];
-
-function Book(name, author, pages,  haveRead) {
+const form = document.querySelector('.book-form');
+function book(name, author, pages,  haveRead) {
     this.name = name;
     this.author = author;
     this.pages = pages;
@@ -8,25 +8,48 @@ function Book(name, author, pages,  haveRead) {
 }
 
 function addBookToLibrary(name, author, pages,  haveRead) {
-    myLibrary.push(new Book(name, author, pages,  haveRead));
-}
-function displayBooks(myLibrary){
-    for (item of myLibrary){
-        const mainTable = document.querySelector('.main-table');
-        const tableRow = document.createElement('tr');
-        mainTable.append(tableRow);
-        for (i = 0; i < Object.keys(item).length; i++){
-            const tableData = document.createElement('td');
-            tableData.textContent = Object.values(item)[i];
-            tableRow.append(tableData);
-        }
+    const newBook = new book(name, author, pages,  haveRead); 
+    myLibrary.push(newBook); //adding to array 
 
+    const mainTable = document.querySelector('.main-table');
+    const tableRow = document.createElement('tr');
+    mainTable.append(tableRow);
+
+    for (i = 0; i < Object.keys(newBook).length; i++){
+        const tableData = document.createElement('td');
+        tableData.textContent = Object.values(newBook)[i];
+        tableRow.append(tableData);
     }
 }
 
+function getFormValues(){
+    const object = Array.from(document.querySelectorAll('.book-form input'))
+    .reduce((acc, input) => ({ ...acc, [input.id]: input.value}), {});
+    return Object.values(object);
+}
 
+const showFormButton = document.querySelector('.show-form-button');
+showFormButton.addEventListener("click", () => {
+    
+    form.hidden = !form.hidden;
+})
 
+const addBookButton = document.querySelector('.add-book-button');
+addBookButton.addEventListener("click", () => {
+    const values = getFormValues();
+    values[3] = (document.querySelector('#read').checked)? "Yes" : "No";
+    addBookToLibrary(values[0], values[1], values[2], values[3]);
+
+    const formValues = document.querySelectorAll('.book-form input');
+    for (let i = 0; i < formValues.length; i++) {
+        formValues[i].value = '';
+    }
+    form.hidden = true;
+
+})
+//Adding few books
 addBookToLibrary("By the Blade","Tim Russel", 255, "No");
 addBookToLibrary("The Hobbit","J.R.R Tolkien", 299, "Yes");
+addBookToLibrary("The Witcher","Andrzej Sapkowski ", 359, "Yes");
 
-displayBooks(myLibrary);
+
